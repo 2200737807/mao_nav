00e7bb37f0dd9051049082b4938901a49fb56fc4
绘念 <71483108+2200737807@users.noreply.github.com>
2025-11-27 21:17:02 +0800

    Update NavHomeView.vue
    3

--- src/views/NavHomeView.vue
+++ src/views/NavHomeView.vue
@@ -55,7 +55,7 @@
       <!-- 左侧边栏底部信息 -->
       <div class="sidebar-footer">
         <a
-          href="https://github.com/maodeyu180/mao_nav"
+          href="https://github.com/2200737807/mao_nav"
           target="_blank"
           rel="noopener noreferrer"
           class="github-link"
