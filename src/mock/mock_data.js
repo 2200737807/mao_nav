b02188ef4d1d5d6c28b9cff4113635e16e018f6e
绘念 <71483108+2200737807@users.noreply.github.com>
2025-12-11 15:39:44 +0800

    chore: 更新导航数据 - 2025/12/11 15:39:44

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -5,342 +5,42 @@ export const mockData = {
       "name": "办公常用",
       "icon": "💻",
       "order": 0,
-      "sites": [
-        {
-          "id": "site-1752649007053",
-          "name": "酒店查询-济州",
-          "url": "https://hotels.ctrip.com/hotels/map?countryId=42&city=737&checkin=2024/04/10&checkout=2024/04/11&optionId=1385458&optionType=Hotel&directSearch=0&optionName=%E6%B5%8E%E5%B7%9E%E4%B9%90%E5%A4%A9%E5%9F%8E%E5%B8%82%E9%85%92%E5%BA%97&display=%E6%B5%8E%E5%B7%9E%E4%B9%90%E5%A4%A9%E5%9F%8E%E5%B8%82%E9%85%92%E5%BA%97%2C%20%E6%B5%8E%E5%B7%9E%E5%B8%82%2C%20%E6%B5%8E%E5%B7%9E%E7%89%B9%E5%88%AB%E8%87%AA%E6%B2%BB%E9%81%93%2C%20%E9%9F%A9%E5%9B%BD&crn=1&adult=1&children=0&searchBoxArg=t&travelPurpose=0&ctm_ref=ix_sb_dl&domestic=0&intl=1",
-          "description": "济州岛酒店查询",
-          "icon": "https://pic1.imgdb.cn/item/6921d6213203f7be0021e2ae.png"
-        },
-        {
-          "id": "linux-do",
-          "name": "酒店查询-首尔",
-          "url": "https://hotels.ctrip.com/hotels/map?countryId=42&city=274&checkin=2024/04/10&checkout=2024/04/11&optionId=274&optionType=IntlCity&directSearch=1&optionName=%E5%A4%A7%E4%BD%BF%E8%B4%B9%E5%B0%94%E8%92%99&display=%E5%A4%A7%E4%BD%BF%E8%B4%B9%E5%B0%94%E8%92%99&crn=1&adult=1&children=0&searchBoxArg=t&travelPurpose=0&ctm_ref=ix_sb_dl&domestic=1&",
-          "description": "首尔酒店查询",
-          "icon": "https://pic1.imgdb.cn/item/6921d6213203f7be0021e2ae.png"
-        },
-        {
-          "id": "site-1752857783656",
-          "name": "酒店查询-东京",
-          "url": "https://hotels.ctrip.com/hotels/map?flexType=1&cityId=228&provinceId=0&districtId=0&countryId=78&destName=%E4%B8%9C%E4%BA%AC&searchType=CT&optionId=228&checkin=2025-11-22&checkout=2025-11-23&crn=1&curr=CNY&locale=zh-CN&v2_mod=79&v2_version=E&showtotalamt=2&sort=1&adult=2&optionType=City&optionId=228",
-          "description": "东京酒店查询",
-          "icon": "https://pic1.imgdb.cn/item/6921d6213203f7be0021e2ae.png"
-        },
-        {
-          "id": "curlconverter",
-          "name": "查航班信息",
-          "url": "https://hotels.ctrip.com/international/?intl=1",
-          "description": "航班信息查询",
-          "icon": "https://pic1.imgdb.cn/item/6921d6213203f7be0021e2ae.png"
-        },
-        {
-          "id": "site-1764246095036",
-          "name": "济州岛天气",
-          "url": "https://zoom.earth/maps/temperature/#view=33.339038,126.564144,10z/model=icon",
-          "description": "全岛温度查询",
-          "icon": "https://zoom.earth/assets/images/icon-apple.3.jpg"
-        },
-        {
-          "id": "site-1764246116082",
-          "name": "济州天气查询",
-          "url": "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%EC%A0%9C%EC%A3%BC%EB%82%A0%EC%94%A8",
-          "description": "济州岛天气查询(需要魔法)",
-          "icon": "https://ssl.pstatic.net/sstatic/search/favicon/favicon_32x32_240820.ico"
-        },
-        {
-          "id": "site-1764246140435",
-          "name": "地址查询",
-          "url": "https://www.google.com/maps",
-          "description": "谷歌地图（需要魔法）",
-          "icon": "https://pic1.imgdb.cn/item/6921d5f43203f7be0021e26a.png"
-        },
-        {
-          "id": "chatgpt",
-          "name": "美团",
-          "url": "https://eo.dianping.com/epassport/home",
-          "description": "美团后台登录",
-          "icon": "https://pic1.imgdb.cn/item/6921d6863203f7be0021e35d.png"
-        },
-        {
-          "id": "claude",
-          "name": "去哪儿网",
-          "url": "https://tts2.piao.qunar.com/home",
-          "description": "去哪儿网后台登录",
-          "icon": "https://pic1.imgdb.cn/item/6921d6bb3203f7be0021e3d8.jpg"
-        },
-        {
-          "id": "midjourney",
-          "name": "客路",
-          "url": "https://merchant.klook.com/zh-CN/login?redirect_url=https%3A%2F%2Fmerchant.klook.com%2Fzh-CN%2Flogin%3Fredirect_url%3Dhttps%253A%252F%252Fmerchant.klook.com%252Fzh-CN%252Fbooking",
-          "description": "客路后台登录",
-          "icon": "https://res.klook.com/image/upload/v1640059532/merchant/favicon_v2.png"
-        },
-        {
-          "id": "copilot",
-          "name": "森林小火车",
-          "url": "https://cp.jejuv.com/login/?REFERER=",
-          "description": "森林小火车出票",
-          "icon": "https://pic1.imgdb.cn/item/6921d6db3203f7be0021e419.png"
-        },
-        {
-          "id": "cursor",
-          "name": "飞猪",
-          "url": "https://seller.fliggy.com/sellerSycm#/?industry=BUS",
-          "description": "飞猪销量查询",
-          "icon": "https://g.alicdn.com/trip/tools/img/favicon.ico?20131130"
-        }
-      ]
+      "sites": []
     },
     {
       "id": "cloud",
       "name": "叶枫常用",
       "icon": "📁",
       "order": 2,
-      "sites": [
-        {
-          "id": "site-1765021708298",
-          "name": "个人引导页",
-          "url": "https://new.103000.vip/",
-          "description": "叶枫个人引导页：",
-          "icon": ""
-        },
-        {
-          "id": "vercel",
-          "name": "个人博客",
-          "url": "http://b.103000.vip/",
-          "description": "个人博客",
-          "icon": "https://pic1.imgdb.cn/item/6921d7003203f7be0021e461.png"
-        },
-        {
-          "id": "cloudflare",
-          "name": "工作博客",
-          "url": "http://43.133.27.159:3001/",
-          "description": "工作博客",
-          "icon": "https://pic1.imgdb.cn/item/6921d7003203f7be0021e461.png"
-        },
-        {
-          "id": "aws",
-          "name": "叶枫影视",
-          "url": "http://43.133.27.159:3000/",
-          "description": "个人影视站",
-          "icon": "https://pic1.imgdb.cn/item/6921d7333203f7be0021e50b.png"
-        },
-        {
-          "id": "aliyun",
-          "name": "微信读书",
-          "url": "https://weread.qq.com/",
-          "description": "VIP拉满，每日白嫖一本书",
-          "icon": "https://rescdn.qqmail.com/node/wr/wrpage/style/images/independent/appleTouchIcon/apple-touch-icon-152x152.png"
-        },
-        {
-          "id": "tencent-cloud",
-          "name": "腾讯云",
-          "url": "https://www.tencentcloud.com/zh?lang=zh&pg=",
-          "description": "腾讯云海外",
-          "icon": "https://pic1.imgdb.cn/item/6921d7cc3203f7be0021e628.jpg"
-        },
-        {
-          "id": "huawei-cloud",
-          "name": "腾讯云",
-          "url": "https://cloud.tencent.com/",
-          "description": "腾讯云国内",
-          "icon": "https://pic1.imgdb.cn/item/6921d7cc3203f7be0021e628.jpg"
-        },
-        {
-          "id": "site-1752644060499",
-          "name": "搬瓦工",
-          "url": "https://bandwagonhost.com/clientarea.php",
-          "description": "个人“墙裂”推荐",
-          "icon": "https://pic1.imgdb.cn/item/6921d7ad3203f7be0021e5f8.png"
-        },
-        {
-          "id": "site-1764247957218",
-          "name": "聚合图床",
-          "url": "https://www.superbed.cn/",
-          "description": "每月免费500张",
-          "icon": "https://www.superbed.cn/favicon.ico"
-        }
-      ]
+      "sites": []
     },
     {
       "id": "dev-tools",
       "name": "AI分享",
       "icon": "📱",
       "order": 3,
-      "sites": [
-        {
-          "id": "github",
-          "name": "cherry-ai",
-          "url": "https://www.cherry-ai.com/",
-          "description": "代码托管平台",
-          "icon": "https://cherry-ai.com/assets/favicon-BmbgeFTf.png"
-        },
-        {
-          "id": "webstorm",
-          "name": "硅基流动",
-          "url": "https://www.siliconflow.cn/",
-          "description": "大模型API",
-          "icon": "https://www.siliconflow.cn/favicon.ico"
-        },
-        {
-          "id": "postman",
-          "name": "cherryin",
-          "url": "https://open.cherryin.ai/",
-          "description": "大模型API",
-          "icon": "https://open.cherryin.ai/logo.png"
-        },
-        {
-          "id": "github",
-          "name": "元宝",
-          "url": "https://yuanbao.tencent.com/chat/naQivTmsDa",
-          "description": "腾讯",
-          "icon": "https://static.yuanbao.tencent.com/m/yuanbao-web/favicon@32.png"
-        },
-        {
-          "id": "vscode",
-          "name": "豆包",
-          "url": "https://www.doubao.com/chat/",
-          "description": "抖音",
-          "icon": "https://lf-flow-web-cdn.doubao.com/obj/flow-doubao/favicon/180x180.png"
-        },
-        {
-          "id": "android-studio",
-          "name": "Seaart",
-          "url": "https://www.seaart.me/zhCN?u_code=4S53L7",
-          "description": "海艺-艺术AI",
-          "icon": "https://image.cdn2.seaart.ai/icon/logo48new.webp"
-        }
-      ]
+      "sites": []
     },
     {
       "id": "community",
       "name": "社区论坛",
       "icon": "👥",
       "order": 4,
-      "sites": [
-        {
-          "id": "stackoverflow",
-          "name": "NGA-晴空村",
-          "url": "https://nga.178.com/thread.php?fid=-7955747",
-          "description": "晴空村啊啊啊啊！",
-          "icon": "https://nga.178.com/favicon.ico"
-        },
-        {
-          "id": "site-1764247698157",
-          "name": "GitHub",
-          "url": "https://github.com/",
-          "description": "找开源！",
-          "icon": "https://pic1.imgdb.cn/item/6921dd2c3203f7be0021eecf.jpg"
-        },
-        {
-          "id": "linuxdo",
-          "name": "Linux.do",
-          "url": "https://linux.do",
-          "description": "Linux与开源技术社区",
-          "icon": "/sitelogo/linux.do.ico"
-        },
-        {
-          "id": "site-1764247725907",
-          "name": "少数派",
-          "url": "https://sspai.com/?utm_source=itab.ink",
-          "description": "少数派？嗯！",
-          "icon": "https://cdn-static.sspai.com/favicon/sspai.ico"
-        },
-        {
-          "id": "nodeseek",
-          "name": "NodeSeek",
-          "url": "https://www.nodeseek.com",
-          "description": "极客技术社区",
-          "icon": "/sitelogo/www.nodeseek.com.ico"
-        },
-        {
-          "id": "v2ex",
-          "name": "V2EX",
-          "url": "https://www.v2ex.com",
-          "description": "创意工作者社区",
-          "icon": "/sitelogo/www.v2ex.com.ico"
-        },
-        {
-          "id": "52pojie",
-          "name": "吾爱破解",
-          "url": "https://www.52pojie.cn/",
-          "description": "软件安全与破解技术论坛",
-          "icon": "/sitelogo/www.52pojie.cn.ico"
-        }
-      ]
+      "sites": []
     },
     {
       "id": "tools",
       "name": "在线工具",
       "icon": "⚙️",
       "order": 5,
-      "sites": [
-        {
-          "id": "json-formatter",
-          "name": "JSON Formatter",
-          "url": "https://jsonformatter.org",
-          "description": "JSON格式化工具",
-          "icon": "/sitelogo/jsonformatter.org.ico"
-        },
-        {
-          "id": "regex101",
-          "name": "Regex101",
-          "url": "https://regex101.com",
-          "description": "正则表达式测试",
-          "icon": "/sitelogo/regex101.com.ico"
-        },
-        {
-          "id": "caniuse",
-          "name": "Can I Use",
-          "url": "https://caniuse.com",
-          "description": "浏览器兼容性查询",
-          "icon": "/sitelogo/caniuse.com.ico"
-        },
-        {
-          "id": "tinypng",
-          "name": "TinyPNG",
-          "url": "https://tinypng.com",
-          "description": "图片压缩工具",
-          "icon": "/sitelogo/tinypng.com.ico"
-        }
-      ]
+      "sites": []
     },
     {
       "id": "design",
       "name": "设计美学",
       "icon": "🎨",
       "order": 6,
-      "sites": [
-        {
-          "id": "figma",
-          "name": "Figma",
-          "url": "https://figma.com",
-          "description": "UI设计工具",
-          "icon": "/sitelogo/figma.com.ico"
-        },
-        {
-          "id": "sketch",
-          "name": "Sketch",
-          "url": "https://www.sketch.com",
-          "description": "界面设计工具",
-          "icon": "/sitelogo/www.sketch.com.ico"
-        },
-        {
-          "id": "canva",
-          "name": "Canva",
-          "url": "https://www.canva.com",
-          "description": "在线设计平台",
-          "icon": "/sitelogo/www.canva.com.ico"
-        },
-        {
-          "id": "adobe-xd",
-          "name": "Adobe XD",
-          "url": "https://www.adobe.com/products/xd.html",
-          "description": "用户体验设计工具",
-          "icon": "/sitelogo/www.adobe.com.ico"
-        }
-      ]
+      "sites": []
     }
   ],
   "title": "叶枫导航"
